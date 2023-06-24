adea2f33b06ef08ea6a2a78f1ab9fd36e429a7b4
Edi Carlos <edi_csilva@hotmail.com>
2023-06-24 09:02:35 -0300

    finishing the javascript code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 
 import data from '../data.json' assert { type: 'json'};
 
+// taking the elements page
 let cardWrapper = document.querySelectorAll('.card__wrapper');
 let componentScore = document.querySelector('.component__score');
+let imageCriterion = document.querySelectorAll('.card__criterion > img');
 
-// an iterator for access cardWrapper content
+// an iterator for access cardWrapper content and imageCriterion
 let iterator = 0;
-let totalScore = 0;
+let totalScore = 0; // this variable if for update the totalScore
 
 for (let card of cardWrapper) {
     let cardTitle = card.querySelector('.card__title');
@@ -20,4 +22,11 @@ for (let card of cardWrapper) {
     iterator++;
 }
 componentScore.innerHTML = Math.round(totalScore/data.length);
+
+// this code is for update the src of the image beside criterion avaliation
+iterator = 0;
+for (let image of imageCriterion) {
+    image.setAttribute('src', data[iterator].icon);
+    iterator++;
+}
 
